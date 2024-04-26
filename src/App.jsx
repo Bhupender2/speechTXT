b829180676446318462c156cb306c5f657b4dfe6
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-27 01:36:44 +0530

    speech recognition done

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,39 @@ import { IoCloudDownloadOutline } from "react-icons/io5";
 import { TiMicrophoneOutline } from "react-icons/ti";
 import { IoStopCircleOutline } from "react-icons/io5";
 import { MdLockReset } from "react-icons/md";
+import 'regenerator-runtime/runtime'
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 function App() {
+
+  const startListening= ()=>SpeechRecognition.startListening({ continuous: true , language:"en-IN"})
+  const {
+    transcript,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
   return (
     <>
       <h1 className="heading">Speech to Text </h1>
       <div className="input_box">
-        <p className="input_box--text"></p>
+        <p className="input_box--text">{transcript}</p>
       </div>
       <div className="card">
-        <button>
+        <button onClick={startListening}>
           <TiMicrophoneOutline size="20px" />
         </button>
-        <button>
+        <button onClick={SpeechRecognition.stopListening}>
           <IoStopCircleOutline size="20px" />
         </button>
         <button>
           <IoCloudDownloadOutline size="20px" />
         </button>
-        <button>
+        <button onClick={resetTranscript}>
           <MdLockReset size="20px" />
         </button>
       </div>
