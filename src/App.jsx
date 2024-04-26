4adc7fc5599d413728ab121fb4e48e103c62b5f3
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-26 20:53:04 +0530

    basic ready without making it responsivey

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,7 @@ function App() {
       <h1>Speech to Text </h1>
       <div className="input_box">
         <p className="input_box--text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-          risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
-          ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula
-          massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci
-          nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl
-          sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae,
-          consequat in, pretium a, enim. Pellentesque congue. Ut in risus
-          volutpat libero pharetra tempor. Cras vestibulum bibendum augue.
-          Praesent egestas leo in pede. Praesent blandities sed, dolor. Cras elementum ultrices diam. Maecenas ligula
-          massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci
-          nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl
-          sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae,
-          consequat in, pretium a, enim. Pellentesque congue. Ut in risus
-          volutpat libero pharetra tempor. Cras vestibulum bibendum augue.
-          Praesent egestas leo in pede. Praesent blandit
+          
         </p>
       </div>
       <div className="card">
